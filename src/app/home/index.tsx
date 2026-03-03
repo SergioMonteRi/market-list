8200f1cb36e1485c3b4e7ec8deaba54f63072f8e
SergioMonteRi <sergioribeiropalermo@gmail.com>
2026-03-03 20:24:46 -0300

    feat: restructure app components and add Babel configuration

---
+++ src/app/home/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native'
+
+import { styles } from './styles'
+
+export const Home = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Market List</Text>
+    </View>
+  )
+}
